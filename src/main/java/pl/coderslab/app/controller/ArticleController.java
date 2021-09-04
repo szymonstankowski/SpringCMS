2c935b094655e74664f77bc53e9d2465b0564779
szymonstankowski <szymonstankowski1@gmail.com>
2021-09-04 22:25:34 +0200

    fixed persistance.xml and new classes

--- src/main/java/pl/coderslab/app/controller/ArticleController.java
+++ src/main/java/pl/coderslab/app/controller/ArticleController.java
@@ -1,9 +1,24 @@
 package pl.coderslab.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.app.dao.ArticleDao;
+import pl.coderslab.app.entity.Article;
 
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
+
+    private ArticleDao articleDao;
+
+    public ArticleController(ArticleDao articleDao){
+        this.articleDao=articleDao;
+    }
+
+    @RequestMapping("/get/{id}")
+    public String getArticle(@PathVariable long id){
+        Article article = articleDao.findById(id);
+        return article.toString();
+    }
 }
